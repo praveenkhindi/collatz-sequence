62d7b81dca373e03132f4173c4748e79a5f74f74
Praveen K <Praveen007cool@gmail.com>
2018-04-01 02:54:35 +0530

    Add files via upload

---
+++ collatz sequence.py
@@ -0,0 +1,15 @@
+print('enter a number :')
+n = (int(input()))
+
+def collatz(number):
+      
+
+      if number % 2 == 0:
+             print(number//2)
+             return number//2
+      elif number % 2 == 1:
+             result = 3*number+1
+             print(result)
+             return(result)
+while n !=1:
+      n = (collatz(n))
